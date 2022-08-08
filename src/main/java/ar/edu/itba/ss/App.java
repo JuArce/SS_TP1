b5a59f1ed492a192557904021b0b16972fa1a5d8
Julian Arce <juarce@itba.edu.ar>
2022-08-08 20:47:40 -0300

    chore: add time measurement
    Co-authored-by: Gian Luca Pecile <gpecile@itba.edu.ar>
    Co-authored-by: Roberto Catalan <rcatalan@itba.edu.ar>

--- src/main/java/ar/edu/itba/ss/App.java
+++ src/main/java/ar/edu/itba/ss/App.java
@@ -5,6 +5,8 @@ import ar.edu.itba.ss.models.Particle;
 import ar.edu.itba.ss.tools.ParticleReader;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,13 +16,19 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class App {
     public static void main(String[] args) {
+        Instant start = Instant.now();
+
         ParticleReader particleReader = new ParticleReader(args[0],args[1]);
         List<Particle> particles = new ArrayList<>();
         Integer l = particleReader.read(particles);
+
         Grid grid = new Grid(l, particles);
         grid.calculateNeighbours();
         particles = grid.getParticles();
         particles.forEach(System.out::println);
+
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end));
         return;
     }
 }
