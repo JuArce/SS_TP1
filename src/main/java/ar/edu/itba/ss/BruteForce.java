2ec98412e46bdae42f47ae24b76b6daeaefaa16b
Julian Arce <juarce@itba.edu.ar>
2022-08-08 20:47:55 -0300

    feat: add brute force implementation
    Co-authored-by: Gian Luca Pecile <gpecile@itba.edu.ar>
    Co-authored-by: Roberto Catalan <rcatalan@itba.edu.ar>

---
+++ src/main/java/ar/edu/itba/ss/BruteForce.java
@@ -0,0 +1,27 @@
+package ar.edu.itba.ss;
+
+import ar.edu.itba.ss.models.Grid;
+import ar.edu.itba.ss.models.Particle;
+import ar.edu.itba.ss.tools.ParticleReader;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        Instant start = Instant.now();
+
+        ParticleReader particleReader = new ParticleReader(args[0],args[1]);
+        List<Particle> particles = new ArrayList<>();
+        Integer l = particleReader.read(particles);
+
+        particles.forEach(p -> p.setNeighbours(particles));
+        particles.forEach(System.out::println);
+
+        Instant end = Instant.now();
+        System.out.println(Duration.between(start, end));
+        return;
+    }
+}
