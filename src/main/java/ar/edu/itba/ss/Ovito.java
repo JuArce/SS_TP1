17dc9af66c0d6513f9878c15cf0e669d19243903
Julian Arce <juarce@itba.edu.ar>
2022-08-09 21:09:31 -0300

    feat: implement OvitoExporter

---
+++ src/main/java/ar/edu/itba/ss/Ovito.java
@@ -0,0 +1,42 @@
+package ar.edu.itba.ss;
+
+import ar.edu.itba.ss.models.Particle;
+import ar.edu.itba.ss.tools.Exporter;
+import ar.edu.itba.ss.tools.OvitoExporter;
+import ar.edu.itba.ss.tools.ParticleReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ovito {
+    public static void main(String[] args) {
+        ParticleReader particleReader = new ParticleReader(args[0], args[1]);
+        List<Particle> particles = new ArrayList<>();
+        particleReader.read(particles, false);
+
+        List<String> lines = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(new File(args[2]).toPath())) {
+            lines = stream
+                    .map(l -> l.split("\n"))
+                    .map(l -> l[0])
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        lines.forEach(l -> {
+            String[] s = l.split(",");
+            particles.stream().filter(p -> p.getId() == Integer.parseInt(s[0])).findFirst().ifPresent(p -> {
+                Arrays.stream(s).forEach(candidate -> p.addNeighbour(particles.stream().filter(p2 -> p2.getId() == Integer.parseInt(candidate)).findFirst().orElseThrow()));
+            });
+        });
+
+        OvitoExporter exporter = new OvitoExporter();
+        exporter.export(args.length < 5 ? "outputOvito.csv" : args[4], new HashSet<>(particles), Integer.parseInt(args[3]));
+    }
+}
---
+++ src/main/java/ar/edu/itba/ss/tools/OvitoExporter.java
@@ -0,0 +1,41 @@
+package ar.edu.itba.ss.tools;
+
+import ar.edu.itba.ss.models.Particle;
+import com.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class OvitoExporter {
+    public void export(String filename, Set<Particle> particles, int selected) {
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter("src/main/output/" + filename), ' ', CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.DEFAULT_LINE_END);
+            writer.writeNext(new String[]{String.valueOf(particles.size())});
+            writer.writeNext(new String[]{});
+
+            particles.forEach(p -> {
+                List<String> line = new ArrayList<>();
+                line.add(String.valueOf(p.getId()));
+                line.add(String.valueOf(p.getRadius()));
+                line.add(String.valueOf(Particle.RC));
+                line.add(String.valueOf(p.getPosition().getX()));
+                line.add(String.valueOf(p.getPosition().getY()));
+                if (p.getId() == selected) {
+                    line.add("S");
+                } else if (p.getNeighbours().stream().map(Particle::getId).anyMatch(id -> id == selected)) {
+                    line.add("N");
+                } else {
+                    line.add("NN");
+                }
+
+                writer.writeNext(line.toArray(new String[0]));
+            });
+            writer.flush();
+        } catch (Exception e) {
+            e.printStackTrace(); //TODO: handle exception
+        }
+
+    }
+}
